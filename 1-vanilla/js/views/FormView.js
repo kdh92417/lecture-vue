b7620b9c354c36a618188a0c1f28e0700e85c81c
kdh92417 <kdh92417@gmail.com>
2021-11-25 18:14:56 +0900

    Show the 'x' button

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -6,14 +6,24 @@ const FormView = Object.create(View)
 
 FormView.setup = function (el) {
   this.init(el)
-  this.inputEl = el.querySelector('[type=text]')
-  this.resetEl = el.querySelector('[type=reset')
-  this.showResetBtn(false)
+  this.inputEl = el.querySelector('[type=text]');
+  this.resetEl = el.querySelector('[type=reset]');
+  this.showResetBtn(false);
+  this.bindEvents();
   return this
 }
 
 FormView.showResetBtn = function (show = true) {
-  this.resetEl.style.display = show ? 'block' : 'none'
+  this.resetEl.style.display = show ? 'block' : 'none';
+}
+
+FormView.bindEvents = function() {
+  // keyup Event : 키를 놓을 때 발생
+  this.inputEl.addEventListener('keyup', e => this.onKeyup(e));
+}
+
+FormView.onKeyup = function () {
+  this.showResetBtn(this.inputEl.value.length);
 }
 
 export default FormView
